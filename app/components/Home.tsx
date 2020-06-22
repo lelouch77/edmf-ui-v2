e5f2923d4a3dcdfdb41d4cff8dfa49216fe5baed
Praveen N <npraveen@live.in>
2020-06-22 21:16:14 +0530

    chore: added bluepint lib 📘

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import DatePicker from 'antd/lib/date-picker';
+import { DateRangePicker } from '@blueprintjs/datetime';
 import routes from '../constants/routes.json';
 import styles from './Home.css';
 
@@ -8,7 +8,7 @@ export default function Home(): JSX.Element {
   return (
     <div className={styles.container} data-tid="container">
       <h2>Home</h2>
-      <DatePicker />
+      <DateRangePicker />
       <br />
       <Link to={routes.COUNTER}>to Counter</Link>
     </div>
