a84612d60699933e67b38503ef3db11c9efe1014
Srikkanth Mohanasundaram <srikkanthm@visualbi.com>
2020-06-27 02:59:13 +0530

    Create Campaign UI

--- app/containers/CampaignsPage.tsx
+++ app/containers/CampaignsPage.tsx
@@ -1,62 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import Campaign from '../features/campaign/Campaign';
-// import { Dialog } from "@blueprintjs/core";
-import APIKeys from '../features/APIKeys'
-const { ipcRenderer } = require('electron');
+import API from '../api/easyDMAPI'
 
 
-export default function CampaignsPage() {
-  const [showModal, setShowModal] = useState(true)
-    // mainProcess.getUserObject().then((userObject: any) => {
-    //   if(!userObject) setShowModal(true)
-    // })
 
+function CampaignsPage() {
 
-    useEffect(() => {
-      console.log('Inside Use Effect......', ipcRenderer)
-      ipcRenderer.send('user:getKeys')
-      //ipcRenderer.send('user:syncFollowers')
-
-    //   mainProcess.setKeys({
-    //     consumer_key: 'xxxxxx',
-    //     consumer_secret: 'xxxxxx',
-    //     access_token_key: 'xxxxxx-xxxxxx',
-    //     access_token_secret: 'xxxxxx'
-    //   }).then((res: any) => alert(JSON.stringify(res)))
-    //  ipcRenderer.once('user:syncFollowers', (e,data) => {
-    //     console.log('received......')
-    //     console.log(data)
-    //     ipcRenderer.send('user:getFollowers')
-    // });
-
-    // ipcRenderer.once('user:getFollowers', (e,data) => {
-    //     console.log('received......')
-    //     console.log(data)
-    // });
-
-    ipcRenderer.once('user:getKeys', (e,data) => {
-        console.log('received......')
-        console.log(data)
-        if(!data){
-          const keys= {
-            consumer_key: 'xxxxxx',
-            consumer_secret: 'xxxxxx',
-            access_token_key: 'xxxxxx-xxxxxx',
-            access_token_secret: 'xxxxxx'
-         }
-          ipcRenderer.send('user:setKeys',keys)
-          //ipcRenderer.send('user:syncFollowers')
-        }
-    });
-
-     ipcRenderer.once('user:setKeys', (e,data) => {
-       if(data){
-         ipcRenderer.send('user:syncFollowers')
-       }
-        
+  useEffect(()=>{
+     API.getAllActiveCampaign().then((allCampaigns)=>{
+       console.log(allCampaigns);
      });
+  },[]);
 
-    },[])
   return (
     <>
       {/* { showModal &&
@@ -74,8 +29,9 @@ export default function CampaignsPage() {
           <APIKeys />
         </Dialog>
       } */}
-
       <Campaign />
     </>
   );
 }
+
+export default CampaignsPage;
--- app/containers/CreateCampaignPage.tsx
+++ app/containers/CreateCampaignPage.tsx
@@ -1,6 +1,42 @@
 import React from 'react';
 import CreateCampaign from '../features/campaign/CreateCampaign';
+import API from '../api/easyDMAPI'
+import { useHistory } from "react-router-dom";
+import routes from '../constants/routes.json';
 
 export default function CreateCampaignPage() {
-  return <CreateCampaign />;
+  const history = useHistory();
+  const newCampaign = {
+    name:"Untitled",
+    description:"",
+    message:"",
+    segmentIds:[],
+    allocated_msg_count:100,
+    scheduled_time:null
+  }
+  //Fetch this from API
+  const allSegments= [];
+
+  async function handleSubmit(newCampaign){
+    //console.log(newCampaign);
+    // const segment = await API.createSegment({
+    //     name: "Segment2",
+    //     description: "This is a test Segment",
+    //     filters: {
+    //         filterType: "AND",
+    //         conditions: [
+    //             {
+    //                 id: "followers_count",
+    //                 operator: "GT",
+    //                 value: 500000
+    //             }
+    //         ]
+    //     }
+    // });
+    // newCampaign.segmentIds = [segment.id]
+    API.createCampaign(newCampaign);
+    //history.push(routes.CAMPAIGNS);
+  }
+
+  return <CreateCampaign campaign={newCampaign} segments={allSegments} onSubmit={handleSubmit}/>;
 }
