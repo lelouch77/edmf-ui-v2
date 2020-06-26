6f4f6f58335bd40a0a07ee26fc1b244014581b13
Akshay Ramesh <akshayrameshbabu@gmail.com>
2020-06-26 22:37:48 +0530

    Removed dispatch from settings page

--- app/containers/SettingsPage.tsx
+++ app/containers/SettingsPage.tsx
@@ -1,15 +1,9 @@
 import React, { useEffect } from 'react';
 import Settings from '../features/settings/Settings';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchSettings } from '../features/settings/SettingsSlice'
+import { useSelector } from 'react-redux';
 
 const SettingsPage = () => {
-  const dispatch = useDispatch();
 	const { settings } = useSelector((state: any) => state.settings)
-	
-	useEffect(() => {
-			dispatch(fetchSettings())
-  }, [])
 
   useEffect(() => {
     console.log('Settings')
