3fc5e10b234108332f79093c5fe1e630c8f136ec
Akshay Ramesh <akshayrameshbabu@gmail.com>
2020-06-27 21:11:55 +0530

    Added tenant filter condition

--- app/features/segments/CreateSegment.tsx
+++ app/features/segments/CreateSegment.tsx
@@ -1,12 +1,100 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../../containers/Header';
 import { AgGridReact } from 'ag-grid-react';
 import { Link } from 'react-router-dom';
 import routes from '../../constants/routes.json'
+import { Input, Select } from 'antd';
+const { Option } = Select;
 
 const defaultColDef = {  sortable: true }
 
+const types: any = {
+	boolean: [
+		{ id: 'equals', label: 'Equals' }
+	],
+	number: [
+		{ id: 'GTE', label: '>=' },
+		{ id: 'LTE', label: '<=' },
+	],
+	string: [
+		{ id: 'contains', label: 'contains' }
+	],
+	columns: [
+		{ id: 'screen_name', label: 'Screen Name', type: 'string', added: false },
+		{ id: 'followers_count', label: 'Followers', type: 'number', added: false },
+		{ id: 'friends_count', label: 'Following', type: 'number', added: false },
+		{ id: 'verified', label: 'Verified', type: 'boolean', added: false },
+		{ id: 'location', label: 'Location', type: 'string', added: false },
+		{ id: 'statuses_count', label: 'Tweets & Retweets', type: 'number', added: false }
+	],
+	joinCondition: [
+		{ id: 'AND', label: 'And' },
+		{ id: 'OR', label: 'Or' }
+	],
+	booleanOptions: [
+		{ id: 'true', label: 'Yes' },
+		{ id: 'false', label: 'No' }
+	]
+}
+
+const TypedDropdown = ({ options = [], value, handleChange, disabled = false, width = 120 }: { dataType: string } | any) => {
+	return (
+		<Select style={{ width }} value={value} onChange={handleChange} disabled={disabled}>
+			{ options.map((option: any) => <Option value={option.id}>{option.label}</Option>) }
+		</Select>
+	)
+}
+
+const FilterRow = ({ index = 0, filter, joinCondition, setJoinCondition, updateFilter }: any) => {
+	
+	const [column, setColumn] = useState(filter.column || null)
+	const [comparatorType, setComparatorType] = useState(filter.conditionType || 'string')
+	const [comparator, setComparator] = useState('')
+	const [constraint, setConstraint] = useState(filter.conditionType || '')
+
+	useEffect(() => {
+		if(column){
+			setComparatorType(types.columns.find((item: any) => item.id == column).type)
+			setComparator('')
+			setConstraint('')
+		}
+	}, [column])
+
+	useEffect(() => {
+		updateFilter({ column, comparator, constraint })
+	}, [column, comparator, constraint])
+
+	return (
+		<div className="flex items-center mb-2">
+			{/* First column - Where / Join Condition Dropdown / Join Condition */}
+			{ index === 0 && <div style={{ width: 80 }}>Where</div> }
+			{ index === 1 && <div><TypedDropdown value={joinCondition} options={types.joinCondition} handleChange={setJoinCondition} width={80} /></div> }
+			{ index > 1 && <div style={{ width: 80 }}>{ joinCondition }</div> }
+			{/* Select Column */}
+			<div className="ml-2"><TypedDropdown options={types.columns} handleChange={setColumn} /></div>
+			{/* comparator */}
+			<div className="ml-2"><TypedDropdown options={types[comparatorType]} value={comparator} handleChange={setComparator} disabled={!column} /></div>
+			{/* constraint */}
+			<div className="ml-2">
+				{ comparatorType == 'boolean' ? 
+					<TypedDropdown options={types.booleanOptions} handleChange={setConstraint} disabled={!column} /> :
+					<Input placeholder="Value" value={constraint} onChange={e => setConstraint(e.target.value)} disabled={!column} />
+				}
+			</div>
+		</div>
+	)
+}
+
 export default () => {
+	const [joinCondition, setJoinCondition] = useState(types.joinCondition[0].id)
+	const [filters, setFilters] = useState([])
+	const createFilterRow = () => setFilters([...filters, {}])
+	const updateFilter = (id, updatedFilter) => setFilters(filters.map((filter, idx) => idx == id ? updatedFilter : filter ))
+	
+	useEffect(() => {
+		console.log(filters)
+	}, [filters])
+
 	return (
 		<div className="w-full">
 			<Header name="Campaigns"/>
@@ -17,10 +105,26 @@ export default () => {
 							Save
 						</button>
 					</Link>
-					<div>Route</div>
+					<div>Segments/Create</div>
 				</div>
 				<div>
-				
+					<div className="p-2">
+						<div className="">Filters</div>
+						<div className="flex flex-col align-left p-5 border-solid border-2 border-gray-300">
+							{ filters.map((filter, index) => 
+								<FilterRow
+									filter={filter}
+									index={index}
+									joinCondition={joinCondition}
+									setJoinCondition={setJoinCondition}
+									updateFilter={(updatedFilter: any) => updateFilter(index, updatedFilter)}
+								/>
+							)}
+							<div onClick={createFilterRow} className="color-indigo-700 hover:underline cursor-pointer">
+								+ Add Filter
+							</div>
+						</div>
+					</div>
 				</div>
 			</main>
 		</div>
