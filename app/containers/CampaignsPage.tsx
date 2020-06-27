c5307a3851217244a91ad4bd885fa43571357cca
Akshay Ramesh <akshayrameshbabu@gmail.com>
2020-06-27 14:11:48 +0530

    Fixed the import issue

--- app/containers/CampaignsPage.tsx
+++ app/containers/CampaignsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Campaign from '../features/campaign/Campaign';
 import API from '../api/easyDMAPI'
 import { useHistory } from "react-router-dom";
