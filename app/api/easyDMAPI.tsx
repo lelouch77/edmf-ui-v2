97f064d890f76e2ebab241cfdafe98e8567e0efb
Akshay Ramesh <akshayrameshbabu@gmail.com>
2020-06-27 13:55:56 +0530

    Added welcome screen

--- app/api/easyDMAPI.tsx
+++ app/api/easyDMAPI.tsx
@@ -32,7 +32,7 @@ export async function getCampaigns(): Promise<CampaignResult>{
 }
 
 
-const routes = ['getFollowers','getPaginatedFollowers', 'getUserObject']
+const routes = ['getFollowers','getPaginatedFollowers', 'getUserObject', 'setKeys']
 
 const exportFunctions: any = {}
 
