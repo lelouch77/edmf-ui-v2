16956572648a9484cf6fdb62e10aa9925390f6d7
Akshay Ramesh <akshayrameshbabu@gmail.com>
2020-06-28 12:20:20 +0530

    Updated the side nav

--- app/containers/Navbar.tsx
+++ app/containers/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes.json';
-import { CampaignsIcon, UserIcon, SettingsIcon } from '../assets/svg'
+import { UserOutlined, InboxOutlined, GroupOutlined, SettingOutlined, CloseOutlined } from '@ant-design/icons'
 
 export default (props: Props) => {
   const [activeState, setActiveState] = useState('')
@@ -19,41 +19,48 @@ export default (props: Props) => {
   const navOptions = [
     {
       label: 'Campaigns',
-      icon: CampaignsIcon,
-      route: routes.HOME
+      icon: InboxOutlined,
+      route: routes.HOME,
     },
     {
       label: 'Segments',
-      icon: UserIcon,
+      icon: GroupOutlined,
       route: routes.SEGMENTS
     },
     {
       label: 'Followers',
-      icon: UserIcon,
+      icon: UserOutlined,
       route: routes.FOLLOWERS
     },
     {
       label: 'Settings',
-      icon: SettingsIcon,
+      icon: SettingOutlined,
       route: routes.SETTINGS
     }
   ]
 
   return (
     <nav aria-label="side bar" aria-orientation="vertical" className="flex-none flex flex-col items-center text-center bg-indigo-700 text-gray-400 border-r min-h-screen">
-      <div className="h-16 flex items-center w-full"></div>
-        <ul>
- 
-          { navOptions.map(nav => {
-              const { label, icon: Icon, route } = nav;
-              const isActive = !!(activeState == route)
-              console.log(activeState, route, isActive)
-              return (
-                <li> <Link to={route} title={label} onClick={() => setActiveState(route)} className={`h-16 px-6 flex items-center hover:text-white w-full cursor-pointer ${ isActive ? 'text-white bg-indigo-800' : '' }`}>
-                    <i className="mx-auto"> <Icon /> </i>
-                </Link> </li>
-              )
-          })}
+      <div className="h-16 flex items-center w-full flex align-center justify-center">
+        <CloseOutlined style={{ fontSize: 28 }} />
+      </div>
+      <ul>
+        { navOptions.map(nav => {
+          const { label, icon: Icon, route } = nav;
+          const isActive = !!(activeState == route)
+          return (
+            <li>
+              <Link
+                to={route}
+                title={label}
+                onClick={() => setActiveState(route)}
+                className={`h-20 px-3 flex flex-col items-center justify-center hover:text-white w-full cursor-pointer ${ isActive ? 'text-white bg-indigo-800' : '' }`}
+              >
+                <i className="mx-auto"> <Icon style={{ fontSize: 20 }} /> </i>
+                <div className="mt-2 text-xs">{ label }</div>
+            </Link> </li>
+          )
+        })}
         </ul>
       <div className="mt-auto h-16 flex items-center w-full"></div>
     </nav>
