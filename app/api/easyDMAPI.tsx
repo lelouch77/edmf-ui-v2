65165e2d01804af078e8099298c6484155cb8bc1
Srikkanth Mohanasundaram <srikkanthm@visualbi.com>
2020-07-06 19:38:45 +0530

    Import mac notifier based on platform

--- app/api/easyDMAPI.tsx
+++ app/api/easyDMAPI.tsx
@@ -1,30 +1,33 @@
 const { ipcRenderer } = require('electron');
 const PUBLIC_METHODS = require('edmf-core/dist/export.details');
-const Notification = require('node-mac-notifier');
 
-const api = (path: string, params: Array<any> = []) => {
-  ipcRenderer.send(path, ...params)
-   return new Promise((resolve, reject) => {
-      ipcRenderer.once(path, (e, data) => {
-          console.log(path, data)
-          if(data.error){
-            reject(data.error);
-          }else{
-            resolve(data);
-          }
-      });
-   });
+if (process.platform === 'darwin') {
+  const Notification = require('node-mac-notifier');
+  ipcRenderer.on('notify', (event, { title, body }) => {
+    new Notification(title, { body });
+  });
 }
 
-ipcRenderer.on('notify', (event, { title, body }) => {
-  new Notification(title, { body });
-});
+const api = (path: string, params: Array<any> = []) => {
+  ipcRenderer.send(path, ...params);
+  return new Promise((resolve, reject) => {
+    ipcRenderer.once(path, (e, data) => {
+      console.log(path, data);
+      if (data.error) {
+        reject(data.error);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
 
-const exportFunctions: any = {}
+const exportFunctions: any = {};
 //console.log(PUBLIC_METHODS);
 PUBLIC_METHODS.default.forEach((functionName: string) => {
-  exportFunctions[functionName] = (...args: Array<any>) => api(functionName, args)
-})
+  exportFunctions[functionName] = (...args: Array<any>) =>
+    api(functionName, args);
+});
 
 export default exportFunctions;
-export { api }
+export { api };
